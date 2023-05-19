8e5865f9de7fd2fa6f04bea21c8c648ba58ce3e2
Viraj Alankar <valankar@gmail.com>
2023-05-19 13:55:05 +0000

    Add more income and expense graphs.

--- i_and_e.py
+++ i_and_e.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import pandas as pd
 import plotly.express as px
+from dateutil.relativedelta import relativedelta
 
 import common
 
@@ -72,6 +73,76 @@ def get_income_expense_df(ledger_df):
     return income_df.join(expense_df, how="outer")
 
 
+def get_average_monthly_top_expenses(ledger_df):
+    """Get average monthly top expenses."""
+    expense_df = get_dataframe(ledger_df, "Expenses")
+    latest_time = expense_df[-1:].index.item()
+    latest_month = latest_time.strftime("%B %Y")
+    latest_month_slice = latest_time.strftime("%Y-%m")
+    categories = []
+    expenses = []
+    for i in (6, 3, 1):
+        i -= 1
+        start_month_slice = (latest_time + relativedelta(months=-i)).strftime("%Y-%m")
+        exp_max_df = (
+            expense_df.loc[start_month_slice:latest_month_slice]
+            .groupby("category")
+            .mean(numeric_only=True)
+            .agg(["idxmax", "max"])
+        )
+        categories.append(exp_max_df.iloc[0].values.item())
+        expenses.append(exp_max_df.iloc[1].values.item())
+    xaxis = [
+        "Last 6 months",
+        "Last 3 months",
+        latest_month,
+    ]
+    top_expenses_df = pd.DataFrame(
+        {"category": categories, "expense": expenses}, index=xaxis
+    )
+    chart = px.bar(
+        top_expenses_df,
+        x=top_expenses_df.index,
+        y="expense",
+        color="category",
+        text_auto=",.0f",
+        title="Average Monthly Top Expenses",
+    )
+    chart.update_xaxes(title_text="", categoryarray=xaxis, categoryorder="array")
+    chart.update_yaxes(title_text="USD")
+    return chart
+
+
+def get_average_monthly_income_expenses_chart(ledger_df):
+    """Get average income and expenses chart."""
+    joined_df = get_income_expense_df(ledger_df).resample("M").sum()
+    incomes = []
+    expenses = []
+    for i in (6, 3, 1):
+        incomes.append(joined_df[-i:].mean()["income"])
+        expenses.append(joined_df[-i:].mean()["expenses"])
+    latest_month = joined_df[-1:].index.item().strftime("%B %Y")
+    i_and_e_avg_df = pd.DataFrame(
+        {"income": incomes, "expense": expenses},
+        index=[
+            "Last 6 months",
+            "Last 3 months",
+            latest_month,
+        ],
+    )
+    chart = px.bar(
+        i_and_e_avg_df,
+        x=i_and_e_avg_df.index,
+        y=i_and_e_avg_df.columns,
+        title="Average Monthly Income and Expenses",
+        barmode="group",
+        text_auto=",.0f",
+    )
+    chart.update_xaxes(title_text="")
+    chart.update_yaxes(title_text="USD")
+    return chart
+
+
 def get_income_expense_yearly_chart(ledger_df):
     """Get yearly income and expense totals chart."""
     joined_df = get_income_expense_df(ledger_df)
@@ -104,6 +175,22 @@ def get_income_expense_monthly_chart(ledger_df):
     return chart
 
 
+def get_yearly_chart(ledger_df, category_prefix, title):
+    """Get yearly income or expense bar chart."""
+    dataframe = get_dataframe(ledger_df, category_prefix)
+    chart = px.histogram(
+        dataframe,
+        x=dataframe.index,
+        y="amount",
+        color="category",
+        title=title,
+        histfunc="sum",
+        category_orders={"category": sorted(dataframe["category"].unique())},
+    )
+    configure_yearly_chart(chart)
+    return chart
+
+
 def get_monthly_chart(ledger_df, category_prefix, title):
     """Get monthly income or expense bar chart."""
     dataframe = get_dataframe(ledger_df, category_prefix)
@@ -117,10 +204,14 @@ def get_monthly_chart(ledger_df, category_prefix, title):
         category_orders={"category": sorted(dataframe["category"].unique())},
     )
     configure_monthly_chart(chart)
+    # Add a trendline that is shifted left to the middle of the month.
     line_df = dataframe.resample("M").sum(numeric_only=True)
+    line_df.index = line_df.index.map(lambda x: pd.to_datetime(x.strftime("%Y-%m-15")))
     line_chart = px.scatter(line_df, x=line_df.index, y="amount", trendline="lowess")
+    line_chart.update_traces(showlegend=True)
     for trace in line_chart.data:
-        chart.add_trace(trace)
+        if trace.mode == "lines":
+            chart.add_trace(trace)
     return chart
 
 
@@ -165,12 +256,15 @@ def main():
     ledger_summarized_df["category"] = ledger_summarized_df["category"].replace(
         r"(.+:.+):.+", r"\1", regex=True
     )
+    get_average_monthly_top_expenses(ledger_df)
 
     with common.temporary_file_move(f"{HTML_PREFIX}/index.html") as output_file:
         write_plots(
             output_file,
             [
                 get_income_expense_yearly_chart(ledger_summarized_df),
+                get_yearly_chart(ledger_summarized_df, "Income", "Yearly Income"),
+                get_yearly_chart(ledger_summarized_df, "Expenses", "Yearly Expenses"),
                 get_income_expense_monthly_chart(ledger_summarized_df),
                 get_monthly_chart(ledger_summarized_df, "Income", "Monthly Income"),
                 get_monthly_chart(ledger_summarized_df, "Expenses", "Monthly Expenses"),
@@ -178,6 +272,8 @@ def main():
                 get_monthly_chart(
                     ledger_df, "Expenses", "Monthly Expenses Categorized"
                 ),
+                get_average_monthly_income_expenses_chart(ledger_df),
+                get_average_monthly_top_expenses(ledger_df),
             ],
         )
 
