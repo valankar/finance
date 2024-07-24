b823b420d0ac82e52c7d2016e59a5a06eb47674c
Viraj Alankar <valankar@gmail.com>
2024-07-24 19:23:52 +0000

    Switch to playwright for IBKR margin.

--- interactive_brokers_margin.py
+++ interactive_brokers_margin.py
@@ -2,28 +2,24 @@
 """Store IBKR margin rate history for USD, CHF."""
 
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
 import common
 
+# pylint: disable=line-too-long
+
 
 def main():
-    """Writes 7 day yield history to CSV file."""
-    url = "https://www.interactivebrokers.com/en/trading/margin-rates.php"
-    soup = BeautifulSoup(requests.get(url, timeout=60).content, "html.parser")
-    usd = float(
-        soup.select(
-            # pylint: disable-next=line-too-long
-            "#interest-schedule > div > div:nth-child(3) > div > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span > span:nth-child(1)"
-        )[0].text.strip("%")
-    )
-    chf = float(
-        soup.select(
-            # pylint: disable-next=line-too-long
-            "#interest-schedule > div > div:nth-child(3) > div > div > table > tbody > tr:nth-child(20) > td:nth-child(3) > span > span:nth-child(1)"
-        )[0].text.strip("%")
-    )
+    """Writes IB margin rates to DB."""
+    usd, chf = [
+        float(x.strip("%"))
+        for x in common.find_xpaths_via_browser(
+            "https://www.interactivebrokers.com/en/trading/margin-rates.php",
+            [
+                '//*[@id="interest-schedule"]/div/div[3]/div/div/table/tbody/tr[1]/td[3]/span/span[1]',
+                '//*[@id="interest-schedule"]/div/div[3]/div/div/table/tbody/tr[20]/td[3]/span/span[1]',
+            ],
+        )
+    ]
     new_df = pd.DataFrame({"USD": usd, "CHF": chf}, index=[pd.Timestamp.now()])
     common.to_sql(new_df, "interactive_brokers_margin_rates")
 
